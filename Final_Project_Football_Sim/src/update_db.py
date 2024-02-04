89b2a46674c5a6fdb096b39929fb2d558259e0a2
McGregor "Mac" M. Ambler <69919992+mmambler@users.noreply.github.com>
2024-02-04 17:54:15 -0500

    Delete Final_Project_Football_Sim directory

--- Final_Project_Football_Sim/src/update_db.py
+++
@@ -1,8 +0,0 @@
-from football_db import FootballDB
-
-# Instantiate DB object
-DB = FootballDB()
-
-# Build tables (ONLY IF ON PURPOSE) and fill db from data in the 'unloaded' folder
-#DB.build_tables(True)
-DB.load_new_data()
